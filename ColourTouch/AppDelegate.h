234c8159a3ce0d9eeef236d4fecb558ff8df392e
Chris Jewell <chrisjohnjewell@gmail.com>
2015-01-31 09:14:36 +1100

    Initial Commit

---
+++ ColourTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColourTouch
+//
+//  Created by Test Mac on 31/01/2015.
+//  Copyright (c) 2015 Chris Jewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColourTouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColourTouch
+//
+//  Created by Test Mac on 31/01/2015.
+//  Copyright (c) 2015 Chris Jewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
